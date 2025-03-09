1bd46469d45ca11b75b18efe42636700a269072b
yejishin95 <yeji2336@naver.com>
2025-03-09 17:14:44 +0900

    spring04 jdbctemplate 생성
    spring03에서 dependency 설정 누락이 있어서 다시 만들어서 설정
    Oracle Driver/Spring Data JDBC/Spring Boot Actuator/Spring
    BootDevTools/Spring Web 체크하여 설정

---
+++ spring04jdbctemplate/src/main/java/com/yeji/spring04/Spring04jdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package com.yeji.spring04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring04jdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring04jdbctemplateApplication.class, args);
+	}
+
+}
---
+++ spring04jdbctemplate/src/test/java/com/yeji/spring04/Spring04jdbctemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeji.spring04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring04jdbctemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
