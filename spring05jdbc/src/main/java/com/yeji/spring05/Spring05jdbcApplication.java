b306ed9fb56572795ab6f4aa4e9a42134a334e1b
yejishin95 <yeji2336@naver.com>
2025-03-09 17:36:24 +0900

    spring05 생성

---
+++ spring05jdbc/src/main/java/com/yeji/spring05/Spring05jdbcApplication.java
@@ -0,0 +1,13 @@
+package com.yeji.spring05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring05jdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring05jdbcApplication.class, args);
+	}
+
+}
---
+++ spring05jdbc/src/test/java/com/yeji/spring05/Spring05jdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeji.spring05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring05jdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
