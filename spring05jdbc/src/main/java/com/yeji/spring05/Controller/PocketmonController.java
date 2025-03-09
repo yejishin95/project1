956ef8947b8de069829d65ee47b7426a91fe31c2
yejishin95 <yeji2336@naver.com>
2025-03-09 17:55:16 +0900

    재업로드

--- spring05jdbc/src/main/java/com/yeji/spring05/Controller/PocketmonController.java
+++ spring05jdbc/src/main/java/com/yeji/spring05/Controller/PocketmonController.java
@@ -3,7 +3,6 @@ package com.yeji.spring05.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yeji.spring05.dao.PocketmonDao;
@@ -15,8 +14,6 @@ public class PocketmonController {
 	@Autowired
 	private PocketmonDao dao;
 	
-	@Autowired
-	private PocketmonDto dto;
 	
 	@RequestMapping("/insert")
 	public String insert(
