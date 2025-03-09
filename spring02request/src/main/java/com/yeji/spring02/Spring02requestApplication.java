36a7d63fabdd38c0524b716ed4dc982661f69b08
yejishin95 <yeji2336@naver.com>
2025-03-09 16:47:54 +0900

    sprint02 생성

---
+++ spring02request/src/main/java/com/yeji/spring02/Spring02requestApplication.java
@@ -0,0 +1,13 @@
+package com.yeji.spring02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring02requestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring02requestApplication.class, args);
+	}
+
+}
---
+++ spring02request/src/test/java/com/yeji/spring02/Spring02requestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeji.spring02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02requestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
