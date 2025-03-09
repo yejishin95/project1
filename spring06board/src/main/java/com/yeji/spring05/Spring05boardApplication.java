ddc55bb8a746033a3b028a86f4fc0eeaf51e3bb8
yejishin95 <yeji2336@naver.com>
2025-03-09 17:56:41 +0900

    spring06 생성

---
+++ spring06board/src/main/java/com/yeji/spring05/Spring05boardApplication.java
@@ -0,0 +1,13 @@
+package com.yeji.spring05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring05boardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring05boardApplication.class, args);
+	}
+
+}
