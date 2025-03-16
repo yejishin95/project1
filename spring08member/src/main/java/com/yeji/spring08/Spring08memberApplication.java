29a2937305f5cb836c07a13320c6c7c7785498b3
yejishin95 <yeji2336@naver.com>
2025-03-16 21:55:28 +0900

    member project 생성

---
+++ spring08member/src/main/java/com/yeji/spring08/Spring08memberApplication.java
@@ -0,0 +1,13 @@
+package com.yeji.spring08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08memberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring08memberApplication.class, args);
+	}
+
+}
