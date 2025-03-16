8a1a370d5dd5f55bf04eea70733e91f42cda60b1
yejishin95 <yeji2336@naver.com>
2025-03-16 18:42:24 +0900

    spring07 book 생성

---
+++ spring07book/src/main/java/com/yeji/spring07/Spring07bookApplication.java
@@ -0,0 +1,13 @@
+package com.yeji.spring07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07bookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07bookApplication.class, args);
+	}
+
+}
---
+++ spring07book/src/test/java/com/yeji/spring07/Spring07bookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeji.spring07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07bookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
